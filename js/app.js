8e4b3e77cb531a27592e44ccf82e3d2db643c9a0
xmkabel <mkabel190@gmail.com>
2024-07-30 15:33:05 +0300

    Nav Bar

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 /** TODO: Build the navigation bar using JavaScript **/
-
+let sections = document.getElementsByTagName('section')
+for (let section of sections){
+    sectionData = section.dataset.nav
+    const navSectionElement = document.createElement('li')
+    navSectionElement.innerHTML = sectionData
+    navSectionElement.classList.add('menu__link')
+    document.getElementById('navbar__list').appendChild(navSectionElement)
+}
 /** TODO: Add smooth scrolling **/
 
 /** TODO: Add an active state **/
