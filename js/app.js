003364d38e322a90af08f26963cf3418542f68a5
xmkabel <mkabel190@gmail.com>
2024-08-14 03:01:06 +0300

    Comment Section with responsive features

--- js/app.js
+++ js/app.js
@@ -20,6 +20,10 @@ function addToNav(section){
     // Appending
     navBar.appendChild(navSectionElement);
 }
+function showNav() {
+    navBar.classList.toggle("navHidden");
+  }
+
 // Loop across the sections for building the navBar
 for (let section of sections){
     addToNav(section);
@@ -62,9 +66,41 @@ function updateActive(){
     removeActive()
     addActive(getClosest())
 }
+// listen for a Scroll to Update Active
 window.addEventListener('scroll',function(){
     updateActive()
-    console.log('You Scrolled')
 });
 
-/** TODO: Add a comment form **/
+// Comment Section
+// Comment Button
+document.getElementById('commentsForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+let commentText = document.getElementById('commentTextInput').value;
+let commentAuthor = document.getElementById('commentAuthorInput').value;
+let commentEmail = document.getElementById('commentEmailInput').value;
+    // Comment Box
+    let newComment = document.createElement('div');
+    newComment.classList.add('comment-box')
+    // Comment Paragraph
+    let newCommentText = document.createElement('p');
+    newCommentText.classList.add('comment-text')
+    newCommentText.textContent = commentText
+    // Comment Email
+    let newCommentEmail = document.createElement('p');
+    newCommentEmail.classList.add('comment-email')
+    newCommentEmail.textContent = commentEmail
+    // Comment Author
+    let newCommentAuthor = document.createElement('p');
+    newCommentAuthor.classList.add('comment-author')
+    newCommentAuthor.textContent = commentAuthor
+    // Appending
+    newComment.appendChild(newCommentText);
+    newComment.appendChild(newCommentAuthor);
+    newComment.appendChild(newCommentEmail);
+    console.log(newCommentAuthor,newCommentEmail,newCommentText)
+    // Append Comment
+    console.log(commentText,commentAuthor,commentEmail)
+    document.getElementById('commentsList').appendChild(newComment);
+    // Clear the form
+    document.getElementById('commentsForm').reset();
+});
